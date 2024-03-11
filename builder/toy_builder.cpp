9896ecf0a150ba476677895f92b87ff22e83e124
Francisco Geiman Thiesen <franciscogthiesen@gmail.com>
2024-03-11 10:47:46 -0700

    Adding support for containers

--- builder/toy_builder.cpp
+++ builder/toy_builder.cpp
@@ -1,10 +1,3 @@
-/* Notes
-    * This is a toy builder that takes a struct and converts it to a json string (supports basic types thanks to Daniel Lemire!)
-    * It uses the new reflection TS and the new ranges TS
-    * It depends on an EDG-Experimental version of the compiler that partially implements the Reflection for C++ 26 proposal (https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2023/p2996r1.html#converting-a-struct-to-a-tuple)
-    * You can run the code on the compiler explorer (https://godbolt.org/z/EKPq7jGK9)
-*/
-
 #include <experimental/meta>
 #include <utility>
 #include <vector>
@@ -60,6 +53,8 @@ struct X
     int b;
     int c;
     std::string d;
+    std::vector<int> e;
+    std::vector<std::string> f;
 };
 
 // The following 3 functions were taken from: https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2023/p2996r1.html#converting-a-struct-to-a-tuple
@@ -157,6 +152,25 @@ constexpr std::string atom(T t) {
     return "\""+escape_json(std::string(t))+"\"";
 }
 
+// Concept that checks if a type is a container but not a string (because string handling is required to be different)
+template<typename T>
+concept ContainerButNotString = requires(T a) {
+    { a.size() } -> std::convertible_to<std::size_t>;
+    { a[std::declval<std::size_t>()] }; // check if elements are accessible for the subscript operator
+} && !std::is_same_v<T, std::string> && !std::is_same_v<T, std::string_view> && !std::is_same_v<T, const char*>;
+
+template<class T>
+requires (ContainerButNotString<T>)
+constexpr std::string atom(T t) {
+    std::string result = "[";
+    for(size_t i = 0; i < t.size(); ++i)
+    {
+        result += atom(t[i]);
+        if(i < t.size() - 1) result += ", ";
+    }
+    result += "]";
+    return result;
+}
 
 template<typename array, class T, size_t... i>
 std::string to_json_string(array& desc, const T& t,  std::index_sequence<i...>)
@@ -203,12 +217,13 @@ std::string to_json_string(Z z) {
 }
 
 int main() {
-    X s1 = {.a = '1', .b = 10, .c = 0, .d = "test string\n\r\""};
+    X s1 = {.a = '1', .b = 10, .c = 0, .d = "test string\n\r\"", .e = {1, 2, 3}, .f = {"ab", "cd", "fg"}};
 
-    auto x = struct_to_tuple<X>(s1);
-    std::cout << x << std::endl;
+    // auto x = struct_to_tuple<X>(s1);
+    // std::cout << x << std::endl;
 
-    printjson(s1);
+    // printjson(s1);
     std::cout << to_json_string(s1) << std::endl;
+
     return 0;
 }
