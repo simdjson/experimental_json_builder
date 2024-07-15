e0e598af7fd021c6d11314d3e80501c7f5e44ac2
Daniel Lemire <dlemire@lemire.me>
2024-07-15 18:18:02 -0400

    more cleaning

--- benchmarks/src/benchmark_serialization.cpp
+++ benchmarks/src/benchmark_serialization.cpp
@@ -13,6 +13,8 @@
 #include "user_profile.hpp"
 #include "custom_serializer.h"
 #include "nlohmann_user_profile.hpp"
+#include "benchmark_helper.hpp"
+
 #if SIMDJSON_BENCH_CPP_REFLECT
 #include "benchmark_reflect_serialization.hpp"
 #endif
@@ -102,46 +104,6 @@ User generate_random_user() {
   return user;
 }
 
-event_collector collector;
-
-template <class function_type>
-event_aggregate bench(const function_type &function, size_t min_repeat = 10,
-                      size_t min_time_ns = 1000000000,
-                      size_t max_repeat = 100) {
-  event_aggregate aggregate{};
-  size_t N = min_repeat;
-  if (N == 0) {
-    N = 1;
-  }
-  for (size_t i = 0; i < N; i++) {
-    std::atomic_thread_fence(std::memory_order_acquire);
-    collector.start();
-    function();
-    std::atomic_thread_fence(std::memory_order_release);
-    event_count allocate_count = collector.end();
-    aggregate << allocate_count;
-    if ((i + 1 == N) && (aggregate.total_elapsed_ns() < min_time_ns) &&
-        (N < max_repeat)) {
-      N *= 10;
-    }
-  }
-  return aggregate;
-}
-
-// Source of the 2 functions below:
-// https://github.com/simdutf/simdutf/blob/master/benchmarks/base64/benchmark_base64.cpp
-void pretty_print(size_t strings, size_t bytes, std::string name, event_aggregate agg) {
-  printf("%-60s : ", name.c_str());
-  printf(" %5.2f MB/s ", bytes * 1000 / agg.elapsed_ns());
-  printf(" %5.2f Ms/s ", strings * 1000 / agg.elapsed_ns());
-  if (collector.has_events()) {
-    printf(" %5.2f GHz ", agg.cycles() / agg.elapsed_ns());
-    printf(" %5.2f c/b ", agg.cycles() / bytes);
-    printf(" %5.2f i/b ", agg.instructions() / bytes);
-    printf(" %5.2f i/c ", agg.instructions() / agg.cycles());
-  }
-  printf("\n");
-}
 
 void bench_custom(std::vector<User> &data) {
   size_t volume = std::accumulate(data.begin(), data.end(), size_t(0), [](size_t a, const User &b) { return a + sizeof(b); });
